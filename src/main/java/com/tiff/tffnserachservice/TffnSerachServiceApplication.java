9851e23acf5e1ee243b71e9d3c0f638a3b248d17
Avani Patel <avanirpatel6@gmail.com>
2022-04-22 22:40:53 -0400

    initial repo

---
+++ src/main/java/com/tiff/tffnserachservice/TffnSerachServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tiff.tffnserachservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TffnSerachServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TffnSerachServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiff/tffnserachservice/TffnSerachServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiff.tffnserachservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TffnSerachServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
