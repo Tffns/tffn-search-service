009ac5e4fda56c9241c4b03d9d58fffb0b3e577f
Avani Patel <avanirpatel6@gmail.com>
2022-04-26 23:39:50 -0400

    TFFN-1 made model classes Embedeable

--- src/main/java/com/tiff/tffnserachservice/model/BusinessHours.java
+++ src/main/java/com/tiff/tffnserachservice/model/BusinessHours.java
@@ -2,11 +2,13 @@ package com.tiff.tffnserachservice.model;
 
 import lombok.Data;
 
+import javax.persistence.Embeddable;
 import java.sql.Time;
 import java.time.DayOfWeek;
 import java.util.Date;
 
 @Data
+@Embeddable
 public class BusinessHours {
     public DayOfWeek startDay;
     public DayOfWeek endDay;
--- src/main/java/com/tiff/tffnserachservice/model/ContactInformation.java
+++ src/main/java/com/tiff/tffnserachservice/model/ContactInformation.java
@@ -2,7 +2,10 @@ package com.tiff.tffnserachservice.model;
 
 import lombok.Data;
 
+import javax.persistence.Embeddable;
+
 @Data
+@Embeddable
 public class ContactInformation {
     public String phoneNumber;
     public String emailAddress;
