2a4ae971a83d5d04e6837c6451586af46cc39875
Avani Patel <avanirpatel6@gmail.com>
2022-05-19 14:02:26 -0400

    TFFN-1- optimized imports.

--- src/main/java/com/tiff/tffnserachservice/service/TiffnerService.java
+++ src/main/java/com/tiff/tffnserachservice/service/TiffnerService.java
@@ -5,7 +5,6 @@ import com.tiff.tffnserachservice.repository.TiffnerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -14,11 +13,11 @@ public class TiffnerService {
     @Autowired
     private TiffnerRepository tiffnerRepository;
 
-    public Optional<Tiffner> findByTiffnerId(Long id){
+    public Optional<Tiffner> findByTiffnerId(Long id) {
         return tiffnerRepository.findByTiffnerId(id);
     }
 
-    public Optional<Tiffner> findByTiffnerName(String name){
+    public Optional<Tiffner> findByTiffnerName(String name) {
         return tiffnerRepository.findByName(name);
     }
 
