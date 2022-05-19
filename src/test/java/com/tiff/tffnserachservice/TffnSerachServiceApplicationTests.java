b007d045ae5f75e95652344f9b10eef6f6c24207
Avani Patel <avanirpatel6@gmail.com>
2022-05-19 14:01:33 -0400

    TFFN-1- added integration testing.

--- src/test/java/com/tiff/tffnserachservice/TffnSerachServiceApplicationTests.java
+++ src/test/java/com/tiff/tffnserachservice/TffnSerachServiceApplicationTests.java
@@ -1,13 +1,22 @@
 package com.tiff.tffnserachservice;
 
+import com.tiff.tffnserachservice.controller.TiffnerController;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class TffnSerachServiceApplicationTests {
+
+	@Autowired
+	TiffnerController tiffnerController;
 
 	@Test
 	void contextLoads() {
+		Assertions.assertNotNull(tiffnerController);
 	}
 
 }
